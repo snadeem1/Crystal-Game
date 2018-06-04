f2ddd73ba46fe97c8fc855c7a4a09f9c30c9fcd0
Sitwat <sitwat.nadeem1@gmail.com>
2018-06-04 15:05:41 -0400

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,20 +11,20 @@ function reset(){
 addup = 0;
 totalScore = 0;
 number = (Math.floor(Math.random() * 50) + 49);
-$("#number").html("Random Number " + number);
-$("#totalscore").html("Total Score " + totalScore);
+$("#number").html(number);
+$("#totalscore").html("Total Score: " + totalScore);
 }
 
 function calculateTotal() {
     if (totalScore == number) {
         wins++;
-        $("#wins").html("Wins " + wins);
+        $("#wins").html("Wins: " + wins);
         reset();
     }
     
     if ( totalScore > number) {
     losses++;
-    $("#losses").html("Losses " + losses);
+    $("#losses").html("Losses: " + losses);
     reset();
     
     }
@@ -37,7 +37,7 @@ $("#ruby").on("click", function(){
     var ruby = $(this).val();
     addup = addup + parseInt(ruby);
     totalScore = totalScore + addup;
-    $("#totalscore").html("Total Score " + totalScore);
+    $("#totalscore").html("Total Score: " + totalScore);
     calculateTotal();
 
 })
@@ -46,7 +46,7 @@ $("#topaz").on("click", function(){
     var topaz = $(this).val();
     addup = addup + parseInt(topaz);
     totalScore = totalScore + addup;
-    $("#totalscore").html("Total Score " + totalScore);
+    $("#totalscore").html("Total Score: " + totalScore);
     calculateTotal();
 
 })
@@ -55,7 +55,7 @@ $("#amber").on("click", function(){
     var amber = $(this).val();
     addup = addup + parseInt(amber);
     totalScore = totalScore + addup;
-    $("#totalscore").html("Total Score " + totalScore);
+    $("#totalscore").html("Total Score: " + totalScore);
     calculateTotal();
 
 
